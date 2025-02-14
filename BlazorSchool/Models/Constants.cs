89e3d821c5d91922985d9261f0b9d99bc4df278c
Logan Pedersen <lpedersen15@my.bcit.ca>
2025-02-14 11:13:20 -0800

    initial commit

---
+++ BlazorSchool/Models/Constants.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace BlazorSchool.Models;
+
+public class Constants
+{
+    public const string BASE_URL = "https://httpwebapilp.azurewebsites.net/";
+}
+
---
+++ Functions2025/Models/School/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Functions2025.Models.School;
+
+public partial class Student
+{
+    public int StudentId { get; set; }
+
+    public string LastName { get; set; } = null!;
+
+    public string FirstName { get; set; } = null!;
+
+    public string School { get; set; } = null!;
+}
